c49282c68bcccb87f0a73c3d74f94237fa0ca3d9
27272 <chensl@hua-cloud.com.cn>
2026-04-10 13:47:46 +0800

    修复 widget-factory 的 WindowInsets 兼容性 - 使用 androidx.core API

--- widget-factory/src/main/java/com/smoothie/widgetFactory/Utilities.kt
+++ widget-factory/src/main/java/com/smoothie/widgetFactory/Utilities.kt
@@ -1,11 +1,18 @@
 package com.smoothie.widgetFactory
 
 import android.graphics.Insets
+import android.graphics.Rect
+import android.os.Build
 import android.view.View
 import android.view.WindowInsets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 fun getInsets(
     view: View,
-    typeMask: Int = WindowInsets.Type.systemBars() or WindowInsets.Type.displayCutout()
-): Insets =
-    view.rootWindowInsets.getInsetsIgnoringVisibility(typeMask)
+    typeMask: Int = WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+): Insets {
+    // 使用兼容 API 21+ 的方式
+    return ViewCompat.getRootWindowInsets(view)?.getInsets(typeMask) 
+        ?: Insets.of(0, 0, 0, 0)
+}
--- widget-factory/src/main/java/com/smoothie/widgetFactory/view/InsetSizeDistributor.kt
+++ widget-factory/src/main/java/com/smoothie/widgetFactory/view/InsetSizeDistributor.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import android.graphics.Insets
 import android.util.AttributeSet
 import android.view.View
-import android.view.WindowInsets
 import android.widget.FrameLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 abstract class InsetSizeDistributor : FrameLayout {
 
@@ -26,9 +27,9 @@ abstract class InsetSizeDistributor : FrameLayout {
     }
 
     private fun applyLayoutParameters() {
-        setOnApplyWindowInsetsListener { view, insets ->
-            val mask = WindowInsets.Type.systemBars() or WindowInsets.Type.displayCutout()
-            val actualInsets = insets.getInsetsIgnoringVisibility(mask)
+        ViewCompat.setOnApplyWindowInsetsListener(this) { view, insets ->
+            val mask = WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+            val actualInsets = insets.getInsets(mask)
             applyInsets(view, actualInsets)
             insets
         }
